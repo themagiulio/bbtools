f5532e3c0809516c1b7f90fff8fc0df2ad38febc
Giulio De Matteis <hello@giuliodematte.is>
2025-07-12 12:23:48 +0200

    Update source.fish

--- source.fish
+++ source.fish
@@ -3,5 +3,8 @@
 # Activate conda environment
 conda activate bioboy
 
+# Set bbtools home
+set -x BBTOOLS_HOME $(realpath $(status dirname))
+
 # Add bin directory to PATH
-fish_add_path $(status dirname)/bin/
+fish_add_path $BBTOOLS_HOME/bin/
